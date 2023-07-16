ea4a25b7e6adc538808933a638ff06d918204949
jirayuwat12 <jirayuwat12@gmail.com>
2023-07-16 18:50:50 +0700

    add project requirement

--- setup.py
+++ setup.py
@@ -3,15 +3,32 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def get_requirements(fname : str):
+    '''
+    extract library name from `fname`
+    '''
+    with open(fname) as fp:
+        reqs = list()
+        for lib in fp.read().split("\n"):
+            # Ignore pypi flags and comments
+            if not lib.startswith("-") or lib.startswith("#"):
+                reqs.append(lib.strip())
+        return reqs
+
+
+install_requires = get_requirements("requirements.txt")
+
+
 setuptools.setup(
     name="ppyNotion",
-    version="0.0.1",
+    version="0.0.1.2",
     author="jirayuwat b.",
     author_email="jirayuwat.work@gmail.com",
     description="Connect to your Notion database with ease.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jirayuwat12/ppyNotion",
+    install_requires = install_requires,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
