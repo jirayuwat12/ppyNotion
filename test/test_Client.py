ca60fb908acf9389f71b3bd8ad292e4a7c53e95b
jirayuwat12 <jirayuwat12@gmail.com>
2023-07-16 18:50:12 +0700

    add test structer

---
+++ test/test_Client.py
@@ -0,0 +1,10 @@
+import unittest
+from ..code import client
+
+class TestClientClass(unittest.TestCase):
+
+    def test_empty_token(self):
+        pass 
+    
+if __name__ == "__main__":
+    unittest.main()
