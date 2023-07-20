5751f24155e1cf199c36c71bc36796cf927b23ad
jirayuwat12 <jirayuwat12@gmail.com>
2023-07-20 23:14:00 +0700

    refactor field to `@property` and check with api

--- code/client.py
+++ code/client.py
@@ -1,23 +1,29 @@
+from typing import Dict
+import requests
+
 class Client:
     
-    __token = ""
-    __header = ""
     __is_set_token = False 
 
-    def __init__(self, token : str = "") -> None:
+    def __init__(self, 
+                 token : str = "",
+                 is_check_token : bool = True
+        ) -> None:
         '''
         class that has the important information to access Notion database
 
             Parameters:
-                token(str) : your integraton token from Notion API
+                token (str) : your integraton token from Notion API
+                is_check_token (bool) : check the token with Notion API -> raise `ValueError` if invalid
 
         You can get `token` from https://www.notion.so/my-integrations in your integration or click "+ New integrations"
         '''
-        if not self.is_valid_token(token):    
-            raise ValueError(f"`token`:{token} is not valid.")
-        self.set_token(token=token)
-
-    def is_valid_token(self, token : str) -> bool:
+        self.set_token(token=token, set_header=True, is_check_token=is_check_token)
+    
+    def is_valid_token(self, 
+                       token : str,
+                       check_with_API : bool = True
+        ) -> bool:
         '''
         Check if `token` is **valid or not** by the following condition
         1. length of `token` not equal to 0
@@ -29,10 +35,21 @@ class Client:
                 True if token is valid
                 False, other
         '''
-        if len(token) != 0 and token.startswith('secret'):
-            return True
-        else:
-            return False
+         
+        #  check with api
+        valid_api = True
+        if check_with_API:
+            response = requests.get(url = r'https://api.notion.com/v1/users/me', headers= {
+                "Authorization": "Bearer" + " " + token,
+                "Content-Type": "application/json",
+                "Notion-Version": "2022-06-28"
+            })
+            valid_api = response.status_code == 200
+    
+        # check form
+        valid_form = len(token) != 0 and token.startswith('secret')
+
+        return valid_form and valid_api
 
     def __str__(self):
         return f'''client token = {self.get_token()}'''
@@ -44,7 +61,8 @@ class Client:
         '''
         return self.__is_set_token
     
-    def get_token(self) -> str:
+    @property
+    def token(self) -> str:
         '''
         get token from class feild
             Return:
@@ -52,7 +70,8 @@ class Client:
         '''
         return self.__token
     
-    def get_header(self) -> dict:
+    @property
+    def header(self) -> dict:
         '''
         get header for API from class feild
             Return:
@@ -60,27 +79,46 @@ class Client:
         '''
         return self.__header
     
-    def set_token(self, token : str, set_header : bool = True) -> None:
+    @token.setter
+    def token(self, var : str) -> None:
+        self.set_token(token=var)
+    
+    @header.setter
+    def header(self, var : Dict) -> None:
+        self.set_token(var['Authorization'].split(' ')[1])
+        self.set_header(
+            Authorization_prefix = var['Authorization'].split(' ')[0],
+            Content_Type = var['Content-Type'],
+            Notion_Version = var['Notion-Version']
+        )
+
+    def set_token(self, 
+                  token : str, 
+                  set_header : bool = False,
+                  is_check_token : bool = True           
+        ) -> None:
         '''
         set `token` if token is valid, raise error in the orther hand.
 
             Parameters:
                 token(str) : your integration token from notion api.
                 set_header(bool) : call `self.set_header` after set token if True.
+                is_check_token(bool) : check token with API
 
         '''
-        if not self.is_valid_token(token):
+        if not self.is_valid_token(token, check_with_API=is_check_token):
             raise ValueError(f"`token`:{token} is not valid.")
         
         self.__token = token
         if set_header:
-            self.set_header()
+            self.set_header(is_check_token=is_check_token)
         self.__is_set_token = True 
 
     def set_header(self,
                     Authorization_prefix : str = "Bearer",
                     Content_Type : str = "application/json",
-                    Notion_Version : str = "2022-06-28"
+                    Notion_Version : str = "2022-06-28",
+                    is_check_token : bool = True
                 ) -> None:
         '''
         set header from token that given to class
@@ -91,11 +129,11 @@ class Client:
                 Notion_Version : Version of Notion API (default = "2022-06-28")
                 
         '''
-        if not self.is_valid_token(self.get_token()):
-            raise ValueError(f"`token`: {self.get_token()} is not valid")
+        if not self.is_valid_token(self.token, check_with_API=is_check_token):
+            raise ValueError(f"`token`: {self.token} is not valid")
         
         self.__header = {
-            "Authorization": Authorization_prefix + " " + self.__token,
+            "Authorization": Authorization_prefix + " " + self.token,
             "Content-Type": Content_Type,
             "Notion-Version": Notion_Version
         }
@@ -117,23 +155,23 @@ if __name__ == "__main__":
 
             # valid string
             token = r"secret....................."
-            obj = Client(token)
-            self.assertTrue(obj.is_valid_token(obj.get_token()))
-            self.assertEqual(token, obj.get_token())
+            obj = Client(token, is_check_token=False)
+            self.assertTrue(obj.is_valid_token(obj.token, check_with_API=False))
+            self.assertEqual(token, obj.token)
         
         def test_geter_setter(self):
             token = r"secret....................."
-            obj = Client(token)
-            self.assertEqual(token, obj.get_token())
-            header = obj.get_header()
+            obj = Client(token, is_check_token=False)
+            self.assertEqual(token, obj.token)
+            header = obj.header
             self.assertEqual(header["Authorization"], "Bearer " + token)
             token2 = r"secret1111111111"
-            obj.set_token(token2)
-            self.assertEqual(obj.get_token(), token2)
-            header2 = obj.get_header()
+            obj.set_token(token2, set_header=True, is_check_token=False)
+            self.assertEqual(obj.token, token2)
+            header2 = obj.header
             self.assertEqual(header2["Authorization"], "Bearer " + token2)
-            obj.set_token(token, False)
-            header = obj.get_header()
+            obj.set_token(token, is_check_token=False)
+            header = obj.header
             self.assertEqual(header["Authorization"], "Bearer " + token2)
     
     unittest.main()
