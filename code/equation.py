680d00cc72d49507629370cf176348ff9f392afd
jirayuwat12 <jirayuwat12@gmail.com>
2023-07-22 13:48:14 +0700

    implement `Equation`

---
+++ code/equation.py
@@ -0,0 +1,44 @@
+'''
+to store equation in Notion Object
+'''
+from typing import Dict, Optional
+from .base_text import BaseText
+
+class Equation(BaseText):
+    '''
+    to store equation in Notion Object
+    '''
+    def __init__(self, obj: Dict = None) -> None:
+        super().__init__(obj)
+        if obj is not None:
+            self.from_object(obj)
+
+    def from_object(self, obj : Dict) -> None:
+        self.expression = obj.get('expression')
+
+
+    def to_object(self) -> Dict:
+        obj = dict()
+
+        return obj
+
+    @property
+    def expression(self) -> str:
+        """
+            return the expression(str) in class
+        """
+        try :
+            self.__expression
+        except AttributeError:
+            self.__expression = str()
+        return self.__expression
+
+
+    @expression.setter
+    def expression(self, var : Optional[str]) -> None:
+        """
+            set value to expression variable, need to be `str` or `None` type
+        """
+        if not isinstance(var, str) and var is not None:
+            raise TypeError("expression must be a `str` or `None` type")
+        self.__expression = var
