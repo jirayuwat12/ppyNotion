1863627db7c39b6d47b43783d2411e0246b9a4f3
Jirayuwat Boonchan <49447426+jirayuwat12@users.noreply.github.com>
2023-07-25 08:03:25 +0000

    test `Parent`

--- ppyNotion/parent.py
+++ ppyNotion/parent.py
@@ -1,20 +1,33 @@
+'''
+parent of Notion API object
+'''
 from typing import Dict, Optional, Union
 from .interface.i_notion_object import INotionObject
 
-class Parent(INotionObject):
 
-    def __init__(self, obj : Dict = None) -> None:
+class Parent(INotionObject):
+    '''
+    Parent class of object came from Notion API
+
+        Parameters:
+            obj(dict) : object from Notion API
+    '''
+
+    def __init__(self, obj: Dict = None) -> None:
         if obj is not None:
             self.from_object(obj)
 
-    def from_object(self, obj : Dict) -> None:
+    def from_object(self, obj: Dict) -> None:
         self.type = obj.get('type')
 
-        self.id = obj.get('id')
-
+        self.data = obj.get(self.type)
 
     def to_object(self) -> Dict:
         obj = dict()
+
+        obj['type'] = self.type
+
+        obj[self.type] = self.data
 
         return obj
 
@@ -23,26 +36,25 @@ class Parent(INotionObject):
         """
             return the type(str) in class
         """
-        try :
+        try:
             self.__type
         except AttributeError:
             self.__type = str()
         return self.__type
 
     @property
-    def id(self) -> Union[str, bool]:
+    def data(self) -> Union[str, bool]:
         """
             return the id(Union[str, bool]) in class
         """
-        try :
+        try:
             self.__id
         except AttributeError:
-            self.__id = Union[str, bool]()
+            self.__id = None
         return self.__id
 
-
     @type.setter
-    def type(self, var : Optional[str]) -> None:
+    def type(self, var: Optional[str]) -> None:
         """
             set value to type variable, need to be `str` or `None` type
         """
@@ -50,12 +62,11 @@ class Parent(INotionObject):
             raise TypeError("type must be a `str` or `None` type")
         self.__type = var
 
-    @id.setter
-    def id(self, var : Optional[Union[str, bool]]) -> None:
+    @data.setter
+    def data(self, var: Optional[Union[str, bool]]) -> None:
         """
             set value to id variable, need to be `Union[str, bool]` or `None` type
         """
         if not isinstance(var, Union[str, bool]) and var is not None:
             raise TypeError("id must be a `Union[str, bool]` or `None` type")
         self.__id = var
-
